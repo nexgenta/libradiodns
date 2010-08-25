adf33c21c1b01e614d4f62b2f5a131e50e19c200
Mo McRoberts <mo@nevali.net>
2010-08-25 23:28:23 +0100

    Refactor, restructure argument handling.

--- cli.c
+++ cli.c
@@ -30,283 +30,609 @@
 
 #include "libradiodns.h"
 
+static int cmd_dns(int argc, char **argv);
+static int cmd_fm(int argc, char **argv);
+static int cmd_dab(int argc, char **argv);
+static int cmd_drm(int argc, char **argv);
+static int cmd_amss(int argc, char **argv);
+static int cmd_hdradio(int argc, char **argv);
+static int cmd_dvb(int argc, char **argv);
+static int cmd_domain(int argc, char **argv);
+static int cmd_target(int argc, char **argv);
+static int cmd_verbose(int argc, char **argv);
+static int cmd_quiet(int argc, char **argv);
+
+struct command {
+	const char *name;
+	int (*fn)(int argc, char **argv);
+	int nargs;
+	int bare:1;
+	int pre:1;
+	int visible:1;
+	const char *usage;
+};
+
+static struct command commands[] = {
+	{ "dns", cmd_dns, 0, 1, 0, 1, "DOMAIN"},
+	{ "fm", cmd_fm, 0, 1, 0, 1, "FREQ PI COUNTRY [SUFFIX]\n - FREQ is specified in units of 10KHz\n - COUNTRY must be an ISO country code or an RDS ECC\n"},
+	{ "vhf", cmd_fm, 0, 1, 0, 0, "FREQ PI COUNTRY [SUFFIX]\n - FREQ is specified in units of 10KHz\n - COUNTRY must be an ISO country code or an RDS ECC\n"},
+	{ "dab", cmd_dab, 0, 1, 0, 1, "SCIDS SID EID ECC [APPTYPE-UATYPE|PA] [SUFFIX]\n - APPTYPE-UATYPE must be specified in hexadecimal, with no prefix (e.g.,\n   1F-3C5)" },
+	{ "drm", cmd_drm, 0, 1, 0, 1, "SID [SUFFIX]" },
+	{ "dvb", cmd_dvb, 0, 1, 0, 1, "ONID TSID SID NID [SUFFIX]" },
+	{ "amss", cmd_amss, 0, 1, 0, 1, "SID [SUFFIX]" },
+	{ "hdradio", cmd_hdradio, 0, 1, 0, 1, "TX CC [SUFFIX]" },
+	{ "target", cmd_target, 0, 0, 0, 1, NULL },
+	{ "domain", cmd_domain, 0, 0, 0, 1, NULL },
+	{ "verbose", cmd_verbose, 0, 0, 1, 1, NULL },
+	{ "quiet", cmd_quiet, 0, 0, 1, 1, NULL },
+	{ NULL, NULL, 0, 0, 0, 0, NULL },
+};
+
+const char *progname;
+static struct command *current_command;
+static radiodns_t *context;
+static int verbose = -1;
+
 static void
-usage(const char *progname)
+usage()
 {
-	fprintf(stderr, "Usage: %s KIND ARGS...\n", progname);
-	fprintf(stderr, " KIND is one of 'domain', 'fm', 'dab', 'drm', amss', or 'hd'\n");
-	fprintf(stderr, " Unless otherwise stated, numeric values can be specified in decimal,\n"
-			" hexadecimal (with a '0x' prefix), or octal (with a '0' prefix).\n\n");
-	fprintf(stderr, "Usage: %s domain DOMAIN\n", progname);
-	fprintf(stderr, "Usage: %s fm FREQ PI COUNTRY [SUFFIX]\n", progname);
-	fprintf(stderr, " - FREQ is specified in units of 10KHz\n"
-			" - COUNTRY must be an ISO country code or an RDS ECC\n");
-	fprintf(stderr, "Usage: %s dab SCIDS SID EID ECC [APPTYPE-UATYPE | PA] [SUFFIX]\n", progname);
-	fprintf(stderr, " - APPTYPE-UATYPE must be specified in hexadecimal, with no prefix (e.g.,\n"
-			"   1F-3C5)\n");
-	fprintf(stderr, "Usage: %s drm SID [SUFFIX]\n", progname);
-	fprintf(stderr, "Usage: %s amss SID [SUFFIX]\n", progname);
-	fprintf(stderr, "Usage: %s hdradio TX CC [SUFFIX]\n", progname);
-	fprintf(stderr, "Usage: %s dvb ONID TSID SID NID [SUFFIX]\n", progname);
-	exit(EXIT_FAILURE);
+	if(current_command)
+	{
+		if(current_command->usage)
+		{
+			fprintf(stderr, "Usage: %s %s\n", current_command->name, current_command->usage);
+		}
+		else
+		{
+			fprintf(stderr, "No usage information available for '%s'\n", current_command->name);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Usage: %s KIND ARGS...\n", progname);
+		fprintf(stderr, " KIND is one of 'dns', 'fm', 'dab', 'drm', amss', or 'hdradio'\n");
+		fprintf(stderr, " Unless otherwise stated, numeric values can be specified in decimal,\n"
+				" hexadecimal (with a '0x' prefix), or octal (with a '0' prefix).\n");
+	}
+}
+
+static int
+check_context(radiodns_t *context)
+{
+	if(context)
+	{
+		return 0;
+	}
+	fprintf(stderr, "%s: failed to create context: %s\n", progname, strerror(errno));
+	return 1;
+}
+
+static int
+cmd_dns(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		usage();
+		return 1;
+	}
+	if(context)
+	{
+		radiodns_destroy(context);
+	}
+	context = radiodns_create(argv[1]);
+	return check_context(context);
+}
+
+static int
+cmd_fm(int argc, char **argv)
+{
+	long freq, pi, cval;
+	const char *country, *suffix;
+	char *endptr, cbuf[16];
+
+	suffix = NULL;
+	if(argc < 4 || argc > 5)
+	{
+		usage();
+		return 1;
+	}
+	endptr = NULL;
+	freq = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing FREQ at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	pi = strtol(argv[2], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing PI at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(isdigit(argv[3][0]) || (argv[3][0] == '0' && argv[3][0] == 'x'))
+	{
+		cval = strtol(argv[3], &endptr, 0);
+		if(endptr && endptr[0])
+		{
+			fprintf(stderr, "%s: error parsing COUNTRY at '%s'\n", argv[0], endptr);
+			usage();
+			return 1;
+		}
+		sprintf(cbuf, "%03x", (int) cval);
+		country = cbuf;
+	}
+	else
+	{
+		country = argv[3];
+	}
+	if(argc == 5)
+	{
+		suffix = argv[4];
+	}
+	if(context)
+	{
+		radiodns_destroy(context);
+	}
+	context = radiodns_create_fm(freq, pi, country, suffix);
+	return check_context(context);
+}
+
+static int
+cmd_dab(int argc, char **argv)
+{
+	const char *suffix;
+	char *endptr;
+	long scids, sid, eid, ecc, apptype, uatype, pa;
+
+	suffix = NULL;
+	if(argc < 5 || argc > 7)
+	{
+		usage();
+		return 1;
+	}
+	scids = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing SCIDS at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	sid = strtol(argv[2], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	eid = strtol(argv[3], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing EID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	ecc = strtol(argv[4], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing ECC at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(argc == 7)
+	{
+		suffix = argv[6];
+	}
+	if(argc >= 6)
+	{
+		if(strlen(argv[5]) <= 6 && strchr(argv[5], '-'))
+		{
+			/* APPTYPE-UATYPE */
+			apptype = strtol(argv[5], &endptr, 16);
+			if(!endptr || endptr[0] != '-')
+			{
+				fprintf(stderr, "%s: error parsing APPTYPE at '%s'\n", argv[0], endptr);
+				usage(argv[0]);
+			}
+			endptr++;
+			uatype = strtol(endptr, &endptr, 16);
+			if(endptr && endptr[0])
+			{
+				fprintf(stderr, "%s: error parsing UATYPE at '%s'\n", argv[0], endptr);
+				usage(argv[0]);
+			}
+			if(context)
+			{
+				radiodns_destroy(context);
+			}
+			context = radiodns_create_dab_xpad(apptype, uatype, scids, sid, eid, ecc, suffix);
+		}
+		else
+		{
+			pa = strtol(argv[5], &endptr, 16);
+			if(endptr && endptr[0])
+			{
+				if(argc == 7)
+				{
+					fprintf(stderr, "%s: error parsing PA at '%s'\n", argv[0], endptr);
+					usage();
+					return 1;
+				}
+				suffix = argv[5];
+				if(context)
+				{
+					radiodns_destroy(context);
+				}
+				context = radiodns_create_dab(scids, sid, eid, ecc, suffix);
+			}
+			else
+			{
+				if(context)
+				{
+					radiodns_destroy(context);
+				}
+				context = radiodns_create_dab_sc(pa, scids, sid, eid, ecc, suffix);
+			}
+		}
+	}
+	else
+	{
+		if(context)
+		{
+			radiodns_destroy(context);
+		}
+		context = radiodns_create_dab(scids, sid, eid, ecc, suffix);
+	}			
+	return check_context(context);
+}
+
+static int
+cmd_drm(int argc, char **argv)
+{
+	long sid;
+	char *endptr;
+	const char *suffix;
+
+	suffix = NULL;
+	if(argc < 2 || argc > 3)
+	{
+		usage();
+		return 1;
+	}
+	sid = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(argc == 4)
+	{
+		suffix = argv[3];
+	}
+	context = radiodns_create_drm(sid, suffix);
+	return check_context(context);
+}
+
+static int
+cmd_amss(int argc, char **argv)
+{
+	long sid;
+	char *endptr;
+	const char *suffix;
+
+	suffix = NULL;
+	if(argc < 2 || argc > 3)
+	{
+		usage();
+		return 1;
+	}
+	sid = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(argc == 4)
+	{
+		suffix = argv[3];
+	}
+	context = radiodns_create_amss(sid, suffix);
+	return check_context(context);
+}
+
+static int
+cmd_hdradio(int argc, char **argv)
+{
+	long tx, cc;
+	char *endptr;
+	const char *suffix;
+
+	suffix = NULL;
+	if(argc < 3 || argc > 4)
+	{
+		usage();
+		return 1;
+	}
+	tx = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing TX at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	cc = strtol(argv[2], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing CC at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(argc == 4)
+	{
+		suffix = argv[3];
+	}
+	context = radiodns_create_hdradio(tx, cc, suffix);
+	return check_context(context);
+}
+
+static int
+cmd_dvb(int argc, char **argv)
+{
+	long onid, tsid, sid, nid;
+	const char *suffix;
+	char *endptr;
+	
+	suffix = NULL;
+	if(argc < 6 || argc > 7)
+	{
+		usage();
+		return 1;
+	}
+	onid = strtol(argv[1], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing ONID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	tsid = strtol(argv[2], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing TSID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	sid = strtol(argv[3], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	nid = strtol(argv[4], &endptr, 0);
+	if(endptr && endptr[0])
+	{
+		fprintf(stderr, "%s: error parsing NID at '%s'\n", argv[0], endptr);
+		usage();
+		return 1;
+	}
+	if(argc == 6)
+	{
+		suffix = argv[5];
+	}
+	context = radiodns_create_dvb(onid, tsid, sid, nid, suffix);
+	return check_context(context);
+}
+
+static int
+cmd_domain(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	if(verbose)
+	{
+		printf("Domain: ");
+	}
+	puts(radiodns_domain(context));
+	return 0;
+}
+
+static int
+cmd_target(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	if(0 > radiodns_resolve_target(context))
+	{
+		fprintf(stderr, "%s: error resolving target: errno = %d, h_errno = %d\n", progname, errno, h_errno);
+		return 1;
+	}
+	if(verbose)
+	{
+		printf("Target: ");
+	}
+	puts(radiodns_target(context));
+	return 0;
+}
+
+static int
+cmd_verbose(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	verbose = 1;
+	return 0;
+}
+
+static int
+cmd_quiet(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	verbose = 0;
+	return 0;
 }
 
 int
 main(int argc, char **argv)
 {
-	radiodns_t *context;
-	long freq, pi, onid, nid, sid, tsid, cval, scids, eid, ecc, apptype, uatype, pa, tx, cc;
-	const char *country, *suffix;
-	char cbuf[16];
-	char *endptr;
-	
-	if(argc < 2)
+	int c, d, r, immediate;
+
+	if(!(progname = argv[0]))
 	{
-		usage(argv[0]);
-	}
-	context = NULL;
-	suffix = NULL;
-	if(!strncmp(argv[1], "domain", strlen(argv[1])))
+		progname = "radiodns";
+	}	
+	immediate = 0;
+	r = 0;
+	for(c = 1; c < argc; c++)
 	{
-		if(argc != 3)
+		if(argv[c][0] != '-')
 		{
-			usage(argv[0]);
+			break;
 		}
-		context = radiodns_create(argv[1]);
-	}
-	else if(!strncmp(argv[1], "fm", strlen(argv[1])) || !strncmp(argv[1], "vhf", strlen(argv[1])))
-	{
-		if(argc < 5 || argc > 6)
+		current_command = NULL;
+		for(d = 0; commands[d].name; d++)
 		{
-			usage(argv[0]);
-		}
-		endptr = NULL;
-		freq = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing FREQ at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		pi = strtol(argv[3], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing PI at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		if(isdigit(argv[4][0]) || (argv[4][0] == '0' && argv[4][0] == 'x'))
-		{
-			cval = strtol(argv[4], &endptr, 0);
-			if(endptr && endptr[0])
+			if(commands[d].bare)
 			{
-				fprintf(stderr, "%s: error parsing COUNTRY at '%s'\n", argv[0], endptr);
-				usage(argv[0]);
+				continue;
 			}
-			sprintf(cbuf, "%03x", (int) cval);
-			country = cbuf;
-		}
-		else
-		{
-			country = argv[4];
-		}
-		if(argc == 5)
-		{
-			suffix = argv[5];
-		}
-		context = radiodns_create_fm(freq, pi, country, suffix);
-	}
-	else if(!strncmp(argv[1], "dab", strlen(argv[1])))
-	{
-		if(argc < 6 || argc > 8)
-		{
-			usage(argv[0]);
-		}
-		scids = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing SCIDS at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		sid = strtol(argv[3], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		eid = strtol(argv[4], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing EID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		ecc = strtol(argv[5], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing ECC at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		if(argc == 8)
-		{
-			suffix = argv[7];
-		}
-		if(argc >= 7)
-		{
-			if(strlen(argv[6]) <= 6 && strchr(argv[6], '-'))
+			if(!strncmp(commands[d].name, &(argv[c][1]), strlen(argv[c]) - 1))
 			{
-				/* APPTYPE-UATYPE */
-				apptype = strtol(argv[6], &endptr, 16);
-				if(!endptr || endptr[0] != '-')
+				if(current_command)
 				{
-					fprintf(stderr, "%s: error parsing APPTYPE at '%s'\n", argv[0], endptr);
-					usage(argv[0]);
+					fprintf(stderr, "%s: '%s' is ambiguous\n", progname, argv[c]);
+					exit(EXIT_FAILURE);
 				}
-				endptr++;
-				uatype = strtol(endptr, &endptr, 16);
-				if(endptr && endptr[0])
-				{
-					fprintf(stderr, "%s: error parsing UATYPE at '%s'\n", argv[0], endptr);
-					usage(argv[0]);
-				}
-				context = radiodns_create_dab_xpad(apptype, uatype, scids, sid, eid, ecc, suffix);
+				current_command = &(commands[d]);
+			}
+		}
+		if(!current_command)
+		{
+			fprintf(stderr, "%s: no such option '%s'\n", progname, argv[c]);
+			exit(EXIT_FAILURE);
+		}
+		if(argc - c - 1 < current_command->nargs)
+		{
+			if(current_command->usage)
+			{
+				fprintf(stderr, "Usage: %s -%s %s\n", progname, current_command->name, current_command->usage);
+			}
+			else if(current_command->nargs == 1)
+			{
+				fprintf(stderr, "%s: option '-%s' requires an argument\n", progname, current_command->name);
 			}
 			else
 			{
-				pa = strtol(argv[6], &endptr, 16);
-				if(endptr && endptr[0])
-				{
-					if(argc == 8)
-					{
-						fprintf(stderr, "%s: error parsing PA at '%s'\n", argv[0], endptr);
-						usage(argv[0]);
-					}
-					suffix = argv[6];
-					context = radiodns_create_dab(scids, sid, eid, ecc, suffix);
-				}
-				else
-				{
-					context = radiodns_create_dab_sc(pa, scids, sid, eid, ecc, suffix);
-				}
+				fprintf(stderr, "%s: option '-%s' requires %d arguments\n", progname, current_command->name, current_command->nargs);
+			}
+			exit(EXIT_FAILURE);
+		}
+		if(current_command->pre)
+		{
+			/* Execute it before doing anything else */
+			if(current_command->fn(current_command->nargs, &(argv[c])))
+			{
+				exit(EXIT_FAILURE);
 			}
 		}
 		else
 		{
-			context = radiodns_create_dab(scids, sid, eid, ecc, suffix);
-		}			
+			/* Execute it immediately after parameter parsing */
+			immediate = 1;
+		}
+		/* Skip any arguments */
+		c += current_command->nargs;
 	}
-	else if(!strncmp(argv[1], "drm", strlen(argv[1])))
+	current_command = NULL;
+	if(c == argc)
 	{
-		if(argc < 3 || argc > 4)
-		{
-			usage(argv[0]);
-		}
-		sid = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		if(argc == 4)
-		{
-			suffix = argv[3];
-		}
-		context = radiodns_create_drm(sid, suffix);
+		/* In a future version, we might flip to an interactive
+		 * mode, but not yet.
+		 */
+		usage();
+		exit(EXIT_FAILURE);
 	}
-	else if(!strncmp(argv[1], "amss", strlen(argv[1])))
+	for(d = 0; commands[d].name; d++)
 	{
-		if(argc < 3 || argc > 4)
+		if(!commands[d].bare)
 		{
-			usage(argv[0]);
+			continue;
 		}
-		sid = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
+		if(!strncmp(commands[d].name, argv[c], strlen(argv[c])))
 		{
-			fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
+			if(current_command)
+			{
+				fprintf(stderr, "%s: '%s' is ambiguous\n", progname, argv[c]);
+				exit(EXIT_FAILURE);
+			}
+			current_command = &(commands[d]);
 		}
-		if(argc == 4)
-		{
-			suffix = argv[3];
-		}
-		context = radiodns_create_amss(sid, suffix);
 	}
-	else if(!strncmp(argv[1], "hdradio", strlen(argv[1])))
+	if(!current_command)
 	{
-		if(argc < 4 || argc > 5)
-		{
-			usage(argv[0]);
-		}
-		tx = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing TX at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		cc = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing CC at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		if(argc == 5)
-		{
-			suffix = argv[4];
-		}
-		context = radiodns_create_hdradio(tx, cc, suffix);
+		fprintf(stderr, "%s: '%s' is not a known kind\n", progname, argv[c]);
+		exit(EXIT_FAILURE);
 	}
-	else if(!strncmp(argv[1], "dvb", strlen(argv[1])))
+	if(current_command->fn(argc - c, &(argv[c])))
 	{
-		if(argc < 6 || argc > 7)
+		exit(EXIT_FAILURE);
+	}
+	if(immediate)
+	{	
+		if(verbose == -1)
 		{
-			usage(argv[0]);
+			verbose = 0;
 		}
-		onid = strtol(argv[2], &endptr, 0);
-		if(endptr && endptr[0])
+		/* Loop argc/argv a second time and execute the ones we skipped earlier */
+		for(c = 1; c < argc; c++)
 		{
-			fprintf(stderr, "%s: error parsing ONID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
+			if(argv[c][0] != '-')
+			{
+				break;
+			}
+			current_command = NULL;
+			for(d = 0; commands[d].name; d++)
+			{
+				if(commands[d].bare)
+				{
+					continue;
+				}
+				if(!strncmp(commands[d].name, &(argv[c][1]), strlen(argv[c]) - 1))
+				{
+					current_command = &(commands[d]);
+				}
+			}		
+			if(!current_command->pre)
+			{
+				if(current_command->fn(current_command->nargs, &(argv[c])))
+				{
+					exit(EXIT_FAILURE);
+				}
+			}
+			/* Skip any arguments */
+			c += current_command->nargs;
 		}
-		tsid = strtol(argv[3], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing TSID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		sid = strtol(argv[4], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing SID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		nid = strtol(argv[5], &endptr, 0);
-		if(endptr && endptr[0])
-		{
-			fprintf(stderr, "%s: error parsing NID at '%s'\n", argv[0], endptr);
-			usage(argv[0]);
-		}
-		if(argc == 7)
-		{
-			suffix = argv[6];
-		}
-		context = radiodns_create_dvb(onid, tsid, sid, nid, suffix);
 	}
 	else
 	{
-		usage(argv[0]);
+		/* Pretend that '-verbose -domain -target' was specified */
+		if(verbose == -1)
+		{
+			verbose = 1;
+		}
+		cmd_domain(0, NULL);
+		r = cmd_target(0, NULL);	   
 	}
-	if(!context)
-	{
-		fprintf(stderr, "%s: failed to create context: %s\n", argv[0], strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	printf("Domain: %s\n", radiodns_domain(context));
-	if(0 > radiodns_resolve_target(context))
-	{
-		fprintf(stderr, "%s: error resolving target: errno = %d, h_errno = %d\n", argv[0], errno, h_errno);
-		radiodns_destroy(context);
-		exit(EXIT_FAILURE);
-	}
-	printf("Target: %s\n", radiodns_target(context));
-	radiodns_destroy(context);
-	return 0;
+	radiodns_destroy(context);	
+	return r;
 }
 
 
